c2adfa1d290e82be5eee84dc62b9aa1a144a214e
SamraBA <bandicsamra@gmail.com>
2021-09-15 22:26:37 +0200

    console update

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,17 +1,19 @@
-function main( numOne, numTwo) {
+function main(numOne, numTwo) {
   // Only change code below this line
-   var numOne = 15;
-   var numTwo = 8;
+   var numOne;
+   var numTwo;
    
 
- sum = numOne + numTwo;
-
-
+   sum = numOne + numTwo;
 
  // Only change code above this line
    return sum;
 
 }
-console.log(main(23, 8)); //change this line
+console.log(main(15, 8)); 
+console.log(main(5, 28)); 
+console.log(main(150, 425)); 
+
+
 module.exports = main;
 
