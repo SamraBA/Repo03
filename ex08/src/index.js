5e9679814022504eddbdf827a7094b2729c65451
SamraBA <bandicsamra@gmail.com>
2021-09-15 21:29:15 +0200

    ex08 done

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+function main( numOne, numTwo) {
+  // Only change code below this line
+   var numOne = 15;
+   var numTwo = 8;
+   
+
+ sum = numOne + numTwo;
+
+
+
+ // Only change code above this line
+   return sum;
+
+}
+console.log(main(23, 8)); //change this line
+module.exports = main;
+
